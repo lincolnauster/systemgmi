e5f3692536ea73c61253e8d53011d9d3e0a66bd5
lincoln auster [they/them] <lincolnauster@gmail.com>
2022-01-17 19:35:29 -0700

    define and fix behavior in com/util/com_until_crlf
    The previous implementation of com_until_crlf had a variety of issues
    --- it was slow and expected that clients formulate input properly. If a
    client requested a resource (or sends nonsense) that doesn't end with a
    CRLF, the implementation would propogate a segfault up to the rest of
    the program without a necessarily clear origin. Part of fixing this is to
    have clear behavior when bad input is recieved. Namely, returning NULL.
    
    In addition, this makes com_until_crlf O(1) in the ideal case
    (well-formulated client input) and O(n) in either degenerate cases
    (extra garbage after the CRLF) or incorrect cases (spec-non-compliant
    clients that don't end their requests with a CRLF).

--- com/util.c
+++ com/util.c
@@ -11,29 +11,26 @@
 char *
 com_until_crlf(struct tls_str *s)
 {
-	char *buf, *peek_buf;
-	int pos, read_return;
+	char *buf;
+	int l;
 
-	pos = 0;
-	read_return = 0;
+	// The spec specifies that the max URL length is 1024 bytes (thus a
+	// 1025-byte buffer with a null terminator.
+	buf = malloc(1025);
 
-	// The spec specifies that the max URL length is 1024 bytes, this is a
-	// good starting number to minimize the possibility of allocations.
-	buf = malloc(1024);
-	peek_buf = malloc(1);
+	l = SSL_read(s->ssl, buf, 1024);
 
-	while (1) {
-		SSL_read(s->ssl, peek_buf, 1);
-		if (*peek_buf == '\r')
-			read_return = 1;
-		else if (read_return && *peek_buf == '\n')
-			break;
-
-		buf[pos++] = *peek_buf;
+	// Look for a CRLF.
+	for (int i = l; i > 1; i--) {
+		if (buf[i - 1] == '\r' && buf[i] == '\n') {
+			buf[i - 1] = '\0';
+			goto out;
+		}
 	}
 
-	buf[pos - 1] = '\0'; // clear out the line ending
-
-	free(peek_buf);
+	// If the PC gets here, then we've not found a CRLF.
+	free(buf);
+	buf = NULL;
+out:
 	return buf;
 }
--- com/util.h
+++ com/util.h
@@ -1,2 +1,3 @@
-/* Get a string from tls_str until a CRLF is read. */
+/* Get a string from tls_str until a CRLF is read. If no such string exists,
+ * com_until_crlf returns NULL. */
 char *com_until_crlf(struct tls_str *);
