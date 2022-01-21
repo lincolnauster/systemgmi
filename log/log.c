156810a0bf32fc40b24d45af00e28b10ac9e00f5
lincoln auster [they/them] <lincolnauster@gmail.com>
2022-01-20 22:52:22 -0700

    use atomics for thread-safe stage logging
    log_stage may be called by any thread at any time, including while
    another stage log is executing (though this is very unlikely in current
    practice). To account for potential race conditions, this patch makes
    cnt_stage an atomic (and unsigned) integer before it becomes a problem.

--- log/log.c
+++ log/log.c
@@ -1,9 +1,10 @@
+#include <stdatomic.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 #include "log.h"
 
-static int cnt_stage = 1;
+static _Atomic unsigned int cnt_stage = 1;
 
 /* Return the number of characters the stage indicator has taken up. */
 static unsigned int stage_offset(void);
@@ -19,7 +20,7 @@ log_stage(const char *msg)
 		// + at the beginning of a line (useful to cover up any console
 		//   input from ^C).
 		"\r\033[1mSTAGE %d \033[0;32m| %s\033[0m\n",
-		cnt_stage++,
+		atomic_fetch_add(&cnt_stage, 1),
 		msg
 	);
 }
@@ -53,8 +54,11 @@ stage_offset(void)
 {
 	unsigned int digits = 1;
 
-	for (int stage_cp = cnt_stage; stage_cp > 0; stage_cp /= 10)
-		digits++;
+	for (
+		int stage_cp = atomic_load(&cnt_stage);
+		stage_cp > 0;
+		stage_cp /= 10
+	) digits++;
 
 	return digits + 6; // offset for the "STAGE: " indicator
 }
